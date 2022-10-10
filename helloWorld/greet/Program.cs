b568d502c75774bf390c839f8e1484fe5613809b
Luskalves <lusca.alves32@gmail.com>
2022-10-10 11:20:03 -0300

    GreetMessage class created

---
+++ helloWorld/greet/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace greet;
+
+public class Greet {
+  public static string GreetMessage(string message) {
+    return message;
+  }
+
+  public static void Main(string[] args) {
+    Console.WriteLine(GreetMessage("Hello, World!"));
+  }
+}
